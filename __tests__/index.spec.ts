53a6aec049fc7319208601584c1a4eab4a665bff
yuzhanglong <loveyzl1123@gmail.com>
2022-05-10 15:28:22 +0800

    ci: add GitHub action ci

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,5 +1,18 @@
-describe('index test', () => {
-  test('assert package name', () => {
-    expect('@attachments/lint-md-parser').toBeTruthy();
+import * as fs from 'fs';
+import * as path from 'path';
+
+const { parseMd } = require('../dist/lint-md-parser');
+
+describe('test lint-md-parser', () => {
+  const mdDemo = fs
+    .readFileSync(path.resolve(__dirname, './common-demo.md'))
+    .toString();
+
+  test('expose method (parseMd) is bundled as function', () => {
+    expect(typeof parseMd).toStrictEqual('function');
+  });
+
+  test('invoke parseMd', () => {
+    expect(parseMd(mdDemo)).toMatchSnapshot();
   });
 });
