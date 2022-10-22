07b5301a53879ba8b8dbbee55cc309bebc1ff4f3
yuzhanglong <loveyzl1123@gmail.com>
2022-10-23 01:20:50 +0800

    feat: add revert ast node api

--- src/parse-md.ts
+++ src/parse-md.ts
@@ -1,8 +1,19 @@
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import frontmatter from 'remark-frontmatter';
+import remarkMdx from 'remark-mdx';
 import remarkGfm from 'remark-gfm';
+import remarkDirective from 'remark-directive';
+import remarkMath from 'remark-math';
 import { MarkdownNode } from './types';
+
+const depsLink = unified()
+  .use(remarkParse)
+  .use(frontmatter)
+  .use(remarkMdx)
+  .use(remarkGfm)
+  .use(remarkDirective)
+  .use(remarkMath);
 
 /**
  * 将 Markdown 解析成 ast
@@ -12,5 +23,16 @@ import { MarkdownNode } from './types';
  * @author YuZhanglong <loveyzl1123@gmail.com>
  */
 export const parseMd = (md: string): MarkdownNode => {
-  return unified().use(remarkParse).use(frontmatter).use(remarkGfm).parse(md);
+  return depsLink.parse(md);
+};
+
+/**
+ * 将 ast 解析成 markdown
+ *
+ * @param {MarkdownNode} node ast 结构
+ * @returns {string} md Markdown 文本
+ * @author YuZhanglong <loveyzl1123@gmail.com>
+ */
+export const revertMdAstNode = (node: MarkdownNode) => {
+  return depsLink.stringify(node);
 };
