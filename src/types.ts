8a92ba8e52c0f175a100e0dec1423cf0a040ee55
luo jiyin <luojiyin@hotmail.com>
2026-06-30 18:33:54 +0800

    chore(api-extractor,parse-md,types,ci): fix tsdoc comments, add @public tags, enable api report, treat warnings as errors (#20)

--- src/types.ts
+++ src/types.ts
@@ -10,43 +10,55 @@ import type {
 
 export type { Code, Link, ListItem, Text } from 'mdast';
 
+/** @public */
 export interface MarkdownDirectiveFields {
   name: string
   attributes?: Record<string, string | null | undefined> | null | undefined
 }
 
+/** @public */
 export interface MarkdownMath extends Literal {
   type: 'math'
   meta?: string | null | undefined
 }
 
+/** @public */
 export interface MarkdownInlineMath extends Literal {
   type: 'inlineMath'
 }
 
+/** @public */
 export interface MarkdownContainerDirective extends Parent, MarkdownDirectiveFields {
   type: 'containerDirective'
   children: Array<BlockContent | DefinitionContent>
 }
 
+/** @public */
 export interface MarkdownLeafDirective extends Parent, MarkdownDirectiveFields {
   type: 'leafDirective'
   children: PhrasingContent[]
 }
 
+/** @public */
 export interface MarkdownTextDirective extends Parent, MarkdownDirectiveFields {
   type: 'textDirective'
   children: PhrasingContent[]
 }
 
+/** @public */
 export type MarkdownRoot = Root;
 
+/** @public */
 export type MarkdownNode = MarkdownRoot | Content;
 
+/** @public */
 export type MarkdownCodeNode = import('mdast').Code;
 
+/** @public */
 export type MarkdownListItemNode = import('mdast').ListItem;
 
+/** @public */
 export type MarkdownLinkNode = import('mdast').Link;
 
+/** @public */
 export type MarkdownTextNode = import('mdast').Text;
